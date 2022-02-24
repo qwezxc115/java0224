6f81c4d1fc9dce4d1e016adaf4d28e018f6822af
qwezxc115 <qwezxc115@naver.com>
2022-02-24 12:38:00 +0900

    newfile

--- src/chap01/CodingTip.java
+++ src/chap01/CodingTip.java
@@ -11,5 +11,15 @@ public class CodingTip {
 		//콘솔 출력 코드 단출 키워드: sysout, sout
 		System.out.println("출력할 텍스트");
 		System.out.println("출력할 텍스트");
+		
+		// 실행 : ctrl + F11
+		
+		// 주석 : ctrl + /
+		
+		// 창크기 변경 : ctrl + m
+		
+		// 새파일 : ctrl + n
+		
+		// 저장 : ctrl + s
 	}
 }
